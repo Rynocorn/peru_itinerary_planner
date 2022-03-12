cac4e2d7dbdc86ade15b7898e01dea38e1daf81f
anonymous anonymous <rjjacobs33@gmail.com>
2022-03-12 06:34:45 +0000

    Direct association between activities and activity_signups

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,6 +1,9 @@
 class Activity < ApplicationRecord
   # Direct associations
 
+  has_many   :activity_signups,
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe Activity, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:activity_signups) }
+
     end
 
     describe "InDirect Associations" do
