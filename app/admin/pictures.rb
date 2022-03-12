f466d2013146ef103004fbb68f3be27ba6f6d033
anonymous anonymous <rjjacobs33@gmail.com>
2022-03-12 06:35:12 +0000

    Whitelist columns in Picture for ActiveAdmin

--- app/admin/pictures.rb
+++ app/admin/pictures.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Picture do
 
+ permit_params :activity_id, :picture, :name
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
